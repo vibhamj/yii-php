42f4505ba1a3fab939896419389597f10ba102a5
Vibha M J <vibha248@gmail.com>
2022-09-23 22:34:34 -0400

    Create and use database
    -Access site using: http://localhost:8080/index.php?r=country%2Findex
    -Used postgres. To create postgres db, go to app, double-click 'postgres' db,
    -CREATE DATABASE yii2basic
    -PDO for php is automatically enabled for newer versions of php
    -Go to db, create table - country with code, name and popualtion as cols
     and insert values

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'pgsql:host=localhost;dbname=yii2basic',
+    'username' => 'vibha',
     'password' => '',
     'charset' => 'utf8',
 
