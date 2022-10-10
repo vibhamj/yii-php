6ed23b92fee080ddfcfaed93e1e7e061f0dcfc09
Vibha M J <vibha248@gmail.com>
2022-10-09 21:21:41 -0400

    oracle db: settings - not yet working

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;dbname=yii2basic',
     'username' => 'vibha',
@@ -11,4 +11,33 @@ return [
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
+];*/
+
+/*return [
+    'yiisoft/db-oracle' => [
+        'dsn' => 'oci:dbname=localhost/XE;charset=AL32UTF8;',
+        'username' => 'system',
+        'password' => 'oracle',
+    ],
+];*/
+
+/*return [
+    'yiisoft/db-oracle' => [
+        'dsn' => 'oci:dbname=192.168.0.14/XE;charset=AL32UTF8;',
+        'username' => 'C##1',
+        'password' => 'admin',
+    ],
+];*/
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'oci:dbname=192.168.0.14:1521/orcl.ht.home;charset=UTF8',
+    'username' => 'C##1',
+    'password' => 'admin',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
 ];
+
